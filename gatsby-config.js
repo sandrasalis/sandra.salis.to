d7c948e6565809f6419373ddcc3a5aa67f4e2250
Jonathan Salis <jon@voxpoplabs.com>
2022-07-28 21:08:02 -0400

    Updating fontAwesome style loading

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,8 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    "gatsby-plugin-root-import"
+    "gatsby-plugin-root-import",
+    `gatsby-plugin-fontawesome-css`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
